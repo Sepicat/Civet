44038fbe99b48974a7b7f96a0cde160656165d00
Harry Twan <gua@desgard.com>
2018-09-18 09:25:18 +0800

    完成 Rank 爬虫数据构建
    Signed-off-by: Harry Twan <gua@desgard.com>

--- civet_spider/civet_spider/items.py
+++ civet_spider/civet_spider/items.py
@@ -12,3 +12,15 @@ class CivetSpiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class GtRankUser(scrapy.Item):
+    rank = scrapy.Field()
+    avatar_url = scrapy.Field()
+    username = scrapy.Field()
+    name = scrapy.Field()
+    location = scrapy.Field()
+    language = scrapy.Field()
+    repo_cnt = scrapy.Field()
+    follower_cnt = scrapy.Field()
+    created_date = scrapy.Field(serializer = str)
+
--- civet_spider/civet_spider/spiders/rank_spider.py
+++ civet_spider/civet_spider/spiders/rank_spider.py
@@ -11,10 +11,20 @@ class GitHubRankSpider(Spider):
 
     def parse(self, response):
         trs = response.xpath("//tr")
+
+        def fetch_text(td):
+            vals = td.xpath(".//text()").extract()
+            return vals[0] if len(vals) > 0 else ""
+
+        def fetch_img(td):
+            vals = td.xpath(".//img/@src").extract()
+            return vals[0] if len(vals) > 0 else ""
+
         for i in range(2, len(trs)):
             tds = trs[i].xpath(".//td")
-            print(tds[0])
-        # filename = response.url.split("/")[-2]
-        # with open(filename, 'wb') as f:
-        #     f.write(response.body)
+            if len(tds) == 9:
+                vals = [fetch_text(tds[j]) if j != 1 else fetch_img(tds[j]) for j in range(0, 9)]
+                print(vals)
+                print("")
+
 
